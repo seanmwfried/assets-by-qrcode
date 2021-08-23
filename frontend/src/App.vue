4a5b12a2ced40bb6ef2c0da9cfdac09bfaef272c
Sean Fried <sean.fried@gmail.com>
2021-08-23 04:47:54 -0500

    Installed naive-ui and began project setup. darkTheme default

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <n-config-provider :theme="darkTheme">
+    <router-view/>
+    <n-global-style />
+  </n-config-provider>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { darkTheme } from 'naive-ui';
 
-#nav {
-  padding: 30px;
+export default {
+  setup() {
+    return {
+      darkTheme
+    }
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import naive from 'naive-ui';
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(naive)
+    .mount('#app');
