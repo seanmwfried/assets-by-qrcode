74bd6c30b46943cea793312e0b8a3a8fa19036fe
sean.fried@gmail.com <sean.fried@gmail.com>
2021-09-03 01:02:30 -0500

    Can now add and remove InputPickers

--- frontend/src/components/InputPicker.vue
+++ frontend/src/components/InputPicker.vue
@@ -1,36 +1,41 @@
 <template>
   <div class="container">
-      <va-select
-        color="primary"
-        :options="options"
-        v-model="selectValue"
-        style="color: black;"
-      />
-      <va-input
-        label="Field Label"
-        v-model="inputName"
-      />
-      <va-input v-if="selectValue == 'text'"
+    <div class="remove-button-container">
+      <va-button class="remove-button" color="#baffc9" text-color="#000" @click="removeSelf">X</va-button>
+    </div>
+    <va-select
+      color="primary"
+      :options="options"
+      v-model="selectValue"
+      style="color: black;"
+    />
+    <va-input
+      label="Field Label"
+      v-model="inputName"
+    />
+    <va-input v-if="selectValue == 'text'"
+      :label="inputLabel"
+      v-model="inputValue"
+    />
+    <va-checkbox v-if="selectValue == 'checkbox'"
+      :label="inputLabel"
+      v-model="inputValue"
+    />
+    <div v-if="selectValue == 'date'">
+      <span>{{inputLabel}}</span>
+      <va-date-picker v-if="selectValue == 'date'"
         :label="inputLabel"
+        color="#fff"
         v-model="inputValue"
       />
-      <va-checkbox v-if="selectValue == 'checkbox'"
-        :label="inputLabel"
-        v-model="inputValue"
-      />
-      <div v-if="selectValue == 'date'">
-        <span>{{inputLabel}}</span>
-        <va-date-picker v-if="selectValue == 'date'"
-          :label="inputLabel"
-          color="#fff"
-          v-model="inputValue"
-        />
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+    props: ['id'],
+
     data(){
         return {
             options: ['text', 'checkbox', 'date'],
@@ -38,6 +43,12 @@ export default {
             inputName: '',
             inputValue: null
         }
+    },
+
+    methods: {
+      removeSelf(){
+        this.$emit('removeSelf', this.id);
+      }
     },
 
     computed: {
@@ -67,6 +78,14 @@ export default {
       display: flex;
       flex-direction: column;
       gap: 5px;
+  }
+
+  .remove-button-container {
+    text-align: right;
+  }
+
+  .remove-button div {
+    padding: 0px;
   }
 
   .va-date-picker {
--- frontend/src/views/CreateAsset.vue
+++ frontend/src/views/CreateAsset.vue
@@ -3,24 +3,48 @@
       <va-card dark stripe stripe-color="#baffc9">
           <va-card-title>Create A New Asset</va-card-title>
           <va-card-content class="grid">
-              <div>
-                <va-button color="#baffc9" text-color="#000">Add Field</va-button>
+              <div class="center-button">
+                <va-button color="#baffc9" text-color="#000" @click="addField">Add Field</va-button>
               </div>
-              <div>
+              <div class="center-button">
                 <va-button color="#baffc9" text-color="#000">Finish</va-button>
               </div>
-              <InputPicker />
+              <InputPicker 
+                v-for="id in inputFields" 
+                :key="id"
+                :id="id"
+                @removeSelf="removeField"
+              />
           </va-card-content>
       </va-card>
   </div>
 </template>
 
 <script>
+import { v4 as uuid } from 'uuid';
 import InputPicker from '../components/InputPicker';
 
 export default {
     components: {
         InputPicker
+    },
+
+    data() {
+        return {
+            inputFields: []
+        }
+    },
+
+    methods: {
+        addField() {
+            //Create unique ID for easy element removal
+            this.inputFields.push(uuid());
+        },
+
+        removeField(inID) {
+            const elementIndex = this.inputFields.indexOf(inID);
+            this.inputFields.splice(elementIndex, 1);
+        }
     }
 }
 </script>
@@ -37,5 +61,9 @@ export default {
         display: grid;
         grid-template-columns: 1fr 1fr;
         gap: 25px;
+    }
+
+    .center-button {
+        text-align: center;
     }
 </style>
