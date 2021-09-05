4f184d0db1c82933b46f8d1925fe692f28824ca4
sean.fried@gmail.com <sean.fried@gmail.com>
2021-09-05 01:16:13 -0500

    Created view for modifying assets

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Landing from '../views/Landing';
 import AssetCreate from '../views/AssetCreate';
 import AssetView from '../views/AssetView';
+import AssetModify from '../views/AssetModify';
 
 const routes = [
   {
     path: '/create',
     name: 'Asset Creation',
     component: AssetCreate
+  },
+  {
+    path: '/modify/:assetID',
+    name: 'Asset Modification',
+    component: AssetModify,
+    props: true
   },
   {
     path: '/asset/:assetID',
---
+++ frontend/src/views/AssetModify.vue
@@ -0,0 +1,13 @@
+<template>
+  <span>Modify</span>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  
+</style>
