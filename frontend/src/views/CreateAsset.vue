86452878a6de53594481ce5efe283b90e12ca752
sean.fried@gmail.com <sean.fried@gmail.com>
2021-09-05 00:30:58 -0500

    Password fields, password data, & error checking

--- frontend/src/views/CreateAsset.vue
+++ frontend/src/views/CreateAsset.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="container">
+    <va-modal v-model="showModal" hide-default-actions>
+      <template #header>
+        <span class="error-heading">ERROR</span>
+      </template>
+      <slot>
+        <p class="error-message">
+          {{ errorMessage }}
+        </p>
+      </slot>
+      <template #footer>
+        <div class="error-action">
+          <va-button color="#baffc9" text-color="#000" @click="showModal = false">OK</va-button>
+        </div>
+      </template>
+    </va-modal>
     <va-card dark stripe stripe-color="#baffc9">
       <va-card-title>Create A New Asset</va-card-title>
       <va-card-content class="grid">
@@ -9,10 +24,17 @@
         <div class="center-button">
           <va-button color="#baffc9" text-color="#000" @click="sendData">Finish</va-button>
         </div>
-        <div class="asset-name-field-container">
+        <div class="asset-required-fields-container">
           <va-input
             label="Asset Name (Required)"
             v-model="assetName"
+          />
+          <va-input
+            label="Asset Password (Required)"
+            v-model="assetPassword"
+          /><va-input
+            label="Repeat Password"
+            v-model="assetPasswordRepeat"
           />
         </div>
         <InputPicker 
@@ -39,7 +61,11 @@
     data() {
       return {
         inputFields: [],
-        assetName: ''
+        assetName: '',
+        assetPassword: '',
+        assetPasswordRepeat: '',
+        showModal: false,
+        errorMessage: "Default error message"
       }
     },
 
@@ -55,15 +81,20 @@
       },
 
       sendData(){
+        //Check required fields for proper data
         if(!this.assetName){
-
-          //TODO: Alert user that assetName is required
-          console.log("Asset Name is empty");
-
-        } else{
-
+          this.errorMessage = "Asset name is required. Please enter an asset name to continue."
+          this.showModal = true;
+        } else if(!this.assetPassword) {
+          this.errorMessage = "Asset password is required. Please enter an asset password and confirm it to continue."
+          this.showModal = true;
+        } else if(this.assetPassword !== this.assetPasswordRepeat){
+          this.errorMessage = "Passwords do not match. Please try again."
+          this.showModal = true;
+        } else {
+          //Gather data to send
           const formDataArray = [];
-          formDataArray.push({assetName: this.assetName});
+          formDataArray.push({ assetName: this.assetName, assetPassword: this.assetPassword });
           this.inputFields.forEach(id => {
             const inputPicker = this.$refs[id];
             formDataArray.push({
@@ -75,6 +106,7 @@
 
           const formData = JSON.stringify({data: formDataArray});
 
+          //Send data & handle response
           fetch(`${process.env.VUE_APP_BACKEND_URL}/asset/create`, {
             method: 'POST',
             mode: 'cors',
@@ -89,13 +121,15 @@
             if(data.result){
               this.$router.push(`/asset/${data.assetID}`);
             } else {
+              this.errorMessage = "There was an error. Please try again."
               console.log(data);
-              //TODO: Handle failure
+              this.showModal = true;
             }
           })
           .catch((error) => {
-            //TODO: Alert user of error, please try again
+            this.errorMessage = "There was an error. Please try again."
             console.error('Error submitting asset data:', error);
+            this.showModal = true;
           })
         }
       }
@@ -141,13 +175,33 @@
     text-align: center;
   }
 
-  .asset-name-field-container {
-    align-self: center;
+  .asset-required-fields-container {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    padding: 10px;
+    border-radius: 10px;
+  }
+
+  .error-heading {
+    color: red;
+    font-size: 2em;
+  }
+  
+  .error-message {
+    margin-top: 10px;
+  }
+
+  .error-action {
+    text-align: right;
+    width: 100%;
   }
 </style>
 
 <style>
-  .remove-button .va-button__content {
-    padding: 0;
+  /* Color prop seems bugged on Vuestic-UI modal component */
+  /* Globally overriding for now */
+  .va-modal__dialog {
+    --va-modal-dialog-background: rgb(68, 70, 66);
   }
 </style>
